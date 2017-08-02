e196fce7ea5e7db10466385f7246e165736b9410
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2017-08-02 19:24:13 +0200

    removed all stuff.

--- scripts/run_subscribe_consumer.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export PROJECT_HOME=$HOME'/Workspace/learning/learning-kafka'
-# echo 'Using PROJECT_HOME='${PROJECT_HOME}
-
-cd ${PROJECT_HOME}
-mvn clean package
-mvn exec:java -Dexec.mainClass="ro.mbe.SubscribeConsumerApp" -Dexec.args="my-subscribe-producer-app"
--- scripts/start_kafka_103.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export KAFKA_HOME=$HOME'/Applications/kafka'
-# echo 'Using KAFKA_HOME='${KAFKA_HOME}
-rm -rfd /tmp/kafka-19103-logs/
-${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server-103.properties
