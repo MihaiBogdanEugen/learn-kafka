21866f374e142938d996fb230ba7c72469ae3674
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2017-08-02 19:24:46 +0200

    Added an Apache Kafka simple producer app.

---
+++ src/main/java/ro/mbe/common/Setup.java
@@ -0,0 +1,39 @@
+package ro.mbe.common;
+
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Setup {
+
+    public static final String[] KafkaServers = new String[] {
+            "localhost:19101",
+            "localhost:19102",
+            "localhost:19103"
+    };
+
+    public static final int PollingTimeout = 1000;
+    public static final int NoOfRecordsToSend = 100;
+    public static final int NoOfRecordsToReceive = 100;
+
+    public static final Map<String, List<Integer>> TopicsAndPartitions = new HashMap<>();
+    static {
+        TopicsAndPartitions.put("sensors.first", Arrays.asList(0));
+        TopicsAndPartitions.put("sensors.second", Arrays.asList(0, 1));
+        TopicsAndPartitions.put("sensors.third", Arrays.asList(0, 1, 2));
+    }
+
+    static Collection<String> getAllTopics() {
+        return TopicsAndPartitions.keySet();
+    }
+
+    static Collection<TopicPartition> getAllPartitions() {
+        return Setup.TopicsAndPartitions.entrySet()
+                .stream()
+                .flatMap(entry -> entry.getValue()
+                        .stream()
+                        .map(partition -> new TopicPartition(entry.getKey(), partition)))
+                .collect(Collectors.toList());
+    }
+}
